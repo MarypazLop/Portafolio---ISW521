feecbcf5772fc765b42fa606a0c50b2586e438fc
MarypazLop <marypazlop24@gmail.com>
2026-06-18 11:17:04 -0600

    feat: Javascript practices and activities documentation

--- Ejercicios/Clase 10/index.js
+++ Ejercicios/Clase 10/index.js
@@ -33,7 +33,7 @@ miVariable = {
 
 const tamano = v8.serialize(miVariable).length; 
 console.log(`El tamano de la variable es: ${tamano} bytes`);
-*/
+
 
 //readline
 
@@ -63,7 +63,79 @@ function validarDatos(nombre) {
 }
 
 iniciar();
+*/
 
+//if
+
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+async function iniciar(edad) {
+    let edadUsuario = await rl.question('Digite su edad: ');
+    if (edadUsuario >= 18) {
+        console.log("Eres adulto");
+    } else if (edadUsuario >= 13 && edadUsuario < 17) {
+        console.log("Eres adolescente");
+    } else {
+        console.log("Eres un niño");
+    }
+    rl.close();
+}
+
+iniciar();
+
+//Ternario
+let edad = 15;
+
+const categoria = edad <= 12 ? "niño" : edad <= 17 ? "adolescente" : edad >= 18 && edad <= 64 ? "adulto" : "adulto mayor";  
+console.log(`La categoría de edad es: ${categoria}`);
+
+//Switch meses
+
+const mes = "enero";
+
+switch (mes) {
+    case "enero":
+        console.log("El mes es enero");
+        break
+    case "febrero":
+        console.log("El mes es febrero");
+        break
+    case "marzo":
+        console.log("El mes es marzo");
+        break
+    case "abril":
+        console.log("El mes es abril");
+        break
+    case "mayo":
+        console.log("El mes es mayo");
+        break
+    case "junio":
+        console.log("El mes es junio");
+        break
+    case "julio":
+        console.log("El mes es julio");
+        break
+    case "agosto":
+        console.log("El mes es agosto");
+        break
+    case "septiembre":
+        console.log("El mes es septiembre");
+        break
+    case "octubre":
+        console.log("El mes es octubre");
+        break
+    case "noviembre":
+        console.log("El mes es noviembre");
+        break
+    case "diciembre":
+        console.log("El mes es diciembre");
+        break
+    default:
+        console.log("Un mes del año");
+}
 /*
 ^ - donde inicia
 $ - donde termina
